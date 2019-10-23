13a120d713044df6f1edef71e14d4678f4c07f9d
karthik <karthik.hanumantharaya@appliedsyntax.io>
2019-10-23 16:23:12 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+  
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div class = "app-body">
+        <div class = "app-text">
+           <text>React Web</text>
+        </div>
+        <form class="app-form" onSubmit={this.handleSubmit}>
+          <label>
+            Name:
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    );
+  }
+}
+
+
+export default App;
